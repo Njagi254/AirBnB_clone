a1ac3c347278f21228692a07fbd62dca34cbb7e5
Njagi254 <samuelkamau09@gmail.com>
2023-03-15 10:35:43 +0300

    state.py correction

--- models/state.py
+++ models/state.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-'''
+"""
 Defines the State class
-'''
+"""
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    '''States for Hbnb application
+    """Represent a state
 
     Attributes:
-        name (str): name of the State
-    '''
+        name (str): The name of the state
 
+    """
