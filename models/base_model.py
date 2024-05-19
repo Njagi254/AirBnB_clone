20506956ac09dfad078af739a3b684b28d393881
Njagi254 <samuelkamau09@gmail.com>
2024-05-19 15:24:35 +0300

    chmod edits

--- models/base_model.py
+++ models/base_model.py
@@ -1,28 +1,46 @@
 #!/usr/bin/python3
-
-"""This script is the base model"""
-
+"""
+BaseModel module that defines all common attributes/methods for other classes.
+"""
 
 import uuid
 from datetime import datetime
 
 class BaseModel:
+    """
+    BaseModel class that defines all common attributes/methods
+    for other classes.
+    """
+    
     def __init__(self):
+        """
+        Initializes a new instance of the BaseModel class.
+        """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
+        """
+        Returns a string representation of the instance.
+        """
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
+        """
+        Updates the public instance attribute updated_at with the current datetime.
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """
+        Returns a dictionary containing all keys/values of __dict__ of the instance.
+        
+        A key __class__ is added to this dictionary with the class name of the object.
+        created_at and updated_at are converted to string objects in ISO format.
+        """
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
-
-
