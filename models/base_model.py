39d5efb6c7ac1a24478eb792450e10c5739993de
Njagi254 <samuelkamau09@gmail.com>
2024-05-19 15:36:44 +0300

    chmod edits

--- models/base_model.py
+++ models/base_model.py
@@ -6,12 +6,13 @@ BaseModel module that defines all common attributes/methods for other classes.
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     """
     BaseModel class that defines all common attributes/methods
     for other classes.
     """
-    
+
     def __init__(self):
         """
         Initializes a new instance of the BaseModel class.
@@ -24,20 +25,25 @@ class BaseModel:
         """
         Returns a string representation of the instance.
         """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """
-        Updates the public instance attribute updated_at with the current datetime.
+        Updates the public instance attribute updated_at with the
+        current datetime.
         """
         self.updated_at = datetime.now()
 
     def to_dict(self):
         """
-        Returns a dictionary containing all keys/values of __dict__ of the instance.
-        
-        A key __class__ is added to this dictionary with the class name of the object.
-        created_at and updated_at are converted to string objects in ISO format.
+        Returns a dictionary containing all keys/values of __dict__
+        of the instance.
+
+        A key __class__ is added to this dictionary with the class
+        name of the object.
+        created_at and updated_at are converted to string objects
+        in ISO format.
         """
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""Defines unittests for models/base_model.py.
-
 """
+Test script for BaseModel class
+"""
+
 from models.base_model import BaseModel
 
 my_model = BaseModel()
@@ -14,4 +15,5 @@ my_model_json = my_model.to_dict()
 print(my_model_json)
 print("JSON of my_model:")
 for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+    print("\t{}: ({}) - {}".format(
+        key, type(my_model_json[key]), my_model_json[key]))
