13e4c663bf850d15633afc82aa0e9e4f4276ee97
Njagi254 <samuelkamau09@gmail.com>
2024-05-19 17:38:19 +0300

    5. Store first object

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,7 @@ BaseModel module that defines all common attributes/methods for other classes.
 
 import uuid
 from datetime import datetime
-
+import models
 
 class BaseModel:
     """
@@ -27,6 +27,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
 
     def __str__(self):
@@ -42,6 +43,7 @@ class BaseModel:
         current datetime.
         """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
